74a5e59eb0f3723329de99d6563541c54170c3b0
Phillupz <phillup.zukowski@gmail.com>
2022-03-24 10:29:46 -0400

    commit tree a4b702e0443c98004172e426631014e9dd12530f

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log('%c HI', 'color: firebrick')
+fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then((response) => response.json())
+    .then((imgData) => renderImg(imgData))
+
+function renderImg(obj){
+    const dogList = document.querySelector('#dog-image-container')
+    obj.message.forEach(dogUrl => {
+        //need to create img element
+        const dogPic = document.createElement('img')
+        //assign dog url to src
+        dogPic.src = dogUrl
+        //add img to div with id
+        document.querySelector('#dog-image-container').appendChild(dogPic)
+    })
+}
+
+fetch("https://dog.ceo/api/breeds/list/all")
+    .then((response) => response.json())
+    .then((breedData) => renderBreedInfo(breedData))
+
+function renderBreedInfo(obj){
+    const dogUl = document.querySelector('#dog-breeds')
+    Object.keys(obj.message).forEach(breed => {
+        const breedList = document.createElement('li')
+        breedList.innerHTML = breed
+        dogUl.appendChild(breedList)
+        breedList.addEventListener('click', function(){
+        breedList.style.color = 'blue'
+        breedList.filter()
+        })
+    })
+}
